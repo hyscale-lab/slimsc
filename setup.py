1ac31c85d8f2bc978a91a2140901e4d34b399ccb
Colin Hong <colin.hong18@gmail.com>
2025-09-03 15:46:29 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='slimsc',
+    version='0.1.0',
+    description='A project for model pruning and analysis.',
+    author='Colin Hong',
+    packages=find_packages(),
+)
